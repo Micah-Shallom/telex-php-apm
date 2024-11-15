694589ec5499a2576a0f276a8fcfc73586241d35
Micah Shallom Bawa <micahshallom@gmail.com>
2024-11-15 03:39:49 +0100

    change

---
+++ config/apm.php
@@ -0,0 +1,7 @@
+<?php
+
+use function env;
+
+return [
+    'webhook_url' => getenv('MYAPM_WEBHOOK_URL') ?: "",
+];
---
+++ src/TelexAPMServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Telex\TelexAPM;
+
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+use Telex\TelexAPM\Middleware\MyApmMiddleware;
+
+
+
+class MyApmServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/apm.php', 'myapm');
+    }
+
+    
+
+    /**
+     * Bootstrap any application services.
+    */
+    public function boot()
+    {
+        // Publish configuration
+        $this->publishes([
+            __DIR__ . '/../config/apm.php' => Config::get('config/apm.php'),
+        ], 'config');
+
+        // Register middleware
+        $this->app['router']->pushMiddlewareToGroup('web', MyApmMiddleware::class);
+    }
+}
+
+
